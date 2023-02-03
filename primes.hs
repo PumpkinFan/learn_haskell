2999f8ec025cc99a1c627e9629acfbf6027d00be
Charles Clark <CharlesRClark30@gmail.com>
2023-02-03 16:41:24 -0500

    improvements after learning more

--- primes.hs
+++ primes.hs
@@ -4,6 +4,7 @@ Doing this to test my early knowledge of haskell. -}
 modUpTo :: Int -> [Int]
 modUpTo x = [x `mod` a | a <- [2..(x-1)]]
 isPrime :: Int -> Bool
-isPrime x = if x == 1 then False else null [a | a <- modUpTo x, a == 0]
+isPrime 1 = False
+isPrime x = null [a | a <- modUpTo x, a == 0]
 getPrimes :: Int -> [Int]
 getPrimes n = [x | x <- [1..n], isPrime x]
--- triangular_numbers.hs
+++ triangular_numbers.hs
@@ -3,8 +3,8 @@ Attempting problem 12 from Project Euler (https://projecteuler.net/problem=12)
 Works, but is very slow. Took several hours to calculate `overNDivisors 500`. 
 -}
 
-getTriangularNum :: (Integral a) => Int -> a
-getTriangularNum n = sum (take n [1..])
+nthTriangularNum :: (Integral a) => Int -> a
+nthTriangularNum n = sum (take n [1..])
 
 getDivisors :: (Integral a) => a -> [a]
 getDivisors x = [y | y <- [1..(x `div` 2)], x `mod` y == 0] ++ [x]
@@ -13,6 +13,6 @@ numOfDivisors :: (Integral a) => a -> Int
 numOfDivisors x = length (getDivisors x)
 
 overNDivisors :: Int -> Int
-overNDivisors n = head (filter (\x -> numOfDivisors x > n) (map getTriangularNum [1..]))
+overNDivisors n = head (filter (\x -> numOfDivisors x > n) (map nthTriangularNum [1..]))
 
 main = putStrLn (show (overNDivisors 500))
